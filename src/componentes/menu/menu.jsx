c30421defac76003a3b13166a5cff621cde72813
Georgee-dev <georgedeveloper951@gmail.com>
2025-03-18 22:15:28 -0600

    configuracion de route app

---
+++ src/componentes/menu/menu.jsx
@@ -0,0 +1,27 @@
+
+
+import Link from "next/link";
+
+const Navbar = () => {
+
+  return (
+    <nav className="p-8 bg-gray-800 text-white">
+      <ul className="flex gap-4">
+      <li className="font-bold pr-8">
+          <Link href="/">Inicio</Link>
+        </li>
+        <li className="font-bold">
+          <Link href="/Login">Login</Link>
+        </li>
+        <li className="font-bold">
+          <Link href="/Usuarios">Usuarios</Link>
+        </li>
+        <li className="font-bold">
+          <Link href="/Proyectos">Proyecto</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
